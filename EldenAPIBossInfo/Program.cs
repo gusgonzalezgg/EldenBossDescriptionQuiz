6564e48e0a37e62fe6e0e8f2549cd35f852909c7
Gustavo Gonzalez <gusgonzalez.gg@gmail.com>
2022-06-02 00:39:14 -0400

    created first logic

---
+++ EldenAPIBossInfo/Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace EldenAPIBossInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string eldenBoss = Console.ReadLine();
+            var eldenURL = $"https://eldenring.fanapis.com/api/bosses?name={eldenBoss}";
+            var eldenClient = new HttpClient();
+            var eldenResponse = eldenClient.GetStringAsync(eldenURL).Result;
+            string eldenInfo = JObject.Parse(eldenResponse).GetValue("data").ToString();
+
+            Console.WriteLine(eldenInfo);
+        }
+    }
+}
